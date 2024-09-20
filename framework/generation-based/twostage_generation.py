76bd50a10dc2926cc5cbe71792c1654b601c6e84
Nianqi-Li <141998560+Nianqi-Li@users.noreply.github.com>
2024-09-20 19:15:20 +0800

    Update twostage_generation.py

--- framework/generation-based/twostage_generation.py
+++ framework/generation-based/twostage_generation.py
@@ -3,9 +3,20 @@ import wikipedia
 import json
 from tqdm import tqdm
 from llm_tools import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", required=True, choices=['chatgpt', 'gpt4', 'gemini'], type=str)
+parser.add_argument("--testset", required=True, type=str)
+args = parser.parse_args()
 
 def llm_predict(text, stop=[]):
-    return chatgpt(text, stop)
+    if args.model == 'chatgpt':
+        return chatgpt(text, stop)
+    elif args.model == 'gpt4':
+        return gpt4(text, stop)
+    elif args.model == 'gemini':
+        return gemini(text, stop)
 
 # get candidate from llm
 def get_candidate(event:dict) -> list:
@@ -94,7 +105,7 @@ def read_jsonl(file_path: str) -> list:
     return data
 
 if __name__ == "__main__":
-    testset = read_jsonl('testset.jsonl')
+    testset = read_jsonl(args.testset)
     for event in tqdm(testset[:]): 
         with open('output.jsonl', 'a+', encoding='utf-8') as f:      
             candidate = get_candidate(event)
